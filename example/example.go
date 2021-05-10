020c89297bcbb937256098789fbc969b97caf37b
liju <liju.g.chacko@gmail.com>
2021-05-10 15:14:58 +0530

    Project name changed from ods2csv to ods2csv_old

--- example/example.go
+++ example/example.go
@@ -1,55 +1,55 @@
 package main
 
 import (
-	"github.com/LIJUCHACKO/ods2csv"
-	"os"
 	"bufio"
 	"fmt"
-)
+	"os"
 
+	"github.com/LIJUCHACKO/ods2csv_old"
+)
 
 // writeLines writes the lines to the given file.
 func writeLines(lines []string, path string) error {
-  file, err := os.Create(path)
-  if err != nil {
-    return err
-  }
-  defer file.Close()
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-  w := bufio.NewWriter(file)
-  for _, line := range lines {
-    fmt.Fprintln(w, line+"\r")
-  }
-  return w.Flush()
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line+"\r")
+	}
+	return w.Flush()
 }
 
 func main() {
-    Filecontent,eerr:=ods.ReadODSFile("test.ods")
-    if eerr != nil {
+	Filecontent, eerr := ods.ReadODSFile("test.ods")
+	if eerr != nil {
 		fmt.Printf("Read : %s\n", eerr)
 		var yes string
 		fmt.Scan(&yes)
 		os.Exit(0)
-    }
-    for _,sheet := range Filecontent.Sheets {
-	outputcontent:=[]string{}
-	for _,row := range sheet.Rows {
-	    rowString := ""
-	    
-	    for _, cell := range row.Cells {
-		  
-		  rowString=rowString+cell.Text+","       //cell.Type or cell.Formula or cell.Value
-	    }
-	    
-	    outputcontent=append(outputcontent,rowString)
 	}
-	fmt.Printf("writing   %s", sheet.Name+".csv\n")
-	if err := writeLines(outputcontent,sheet.Name+".csv" ); err != nil {
-		fmt.Printf("writing: %s", err)
-		var yes string
-		fmt.Scan(&yes)
-		return
+	for _, sheet := range Filecontent.Sheets {
+		outputcontent := []string{}
+		for _, row := range sheet.Rows {
+			rowString := ""
+
+			for _, cell := range row.Cells {
+
+				rowString = rowString + cell.Text + "," //cell.Type or cell.Formula or cell.Value
+			}
+
+			outputcontent = append(outputcontent, rowString)
+		}
+		fmt.Printf("writing   %s", sheet.Name+".csv\n")
+		if err := writeLines(outputcontent, sheet.Name+".csv"); err != nil {
+			fmt.Printf("writing: %s", err)
+			var yes string
+			fmt.Scan(&yes)
+			return
+		}
 	}
-    }
-	    
+
 }
